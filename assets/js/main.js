affb64fb0b8f3478c462db7284999a8ee5807172
Ramcod <ramcoding9@gmail.com>
2022-01-09 14:11:25 +0000

    Finalise pop up JS and test

--- assets/js/main.js
+++ assets/js/main.js
@@ -48,12 +48,12 @@ function initMap() {
   });
 }
 $(document).ready(function () {
-  // Activate Carousel with a specified interval
+  // Carousel with a timed interval
   $("#myCarousel").carousel({
     interval: 6000
   });
 
-  // Enable Carousel Indicators
+  // Carousel Indicators- arrows
   $(".item1").click(function () {
     $("#myCarousel").carousel(0);
   });
@@ -64,7 +64,7 @@ $(document).ready(function () {
     $("#myCarousel").carousel(2);
   });
 
-  // Enable Carousel Controls
+  // Carousel Controls
   $(".carousel-control-prev").click(function () {
     $("#myCarousel").carousel("prev");
   });
@@ -73,10 +73,129 @@ $(document).ready(function () {
   });
 });
 
-// Pop up controls
+// Pop up controls for link to wine club
 const button = document.querySelector("button");
 const popup = document.querySelector(".popup-background");
+const close = document.querySelector(".popup-exit");
 
 button.addEventListener("click", () => {
 popup.style.display = "block";
 });
+
+close.addEventListener("click", () => {
+  popup.style.display = "none";
+  });
+  
+popup.addEventListener("click", () => {
+    popup.style.display = "none";
+    });
+
+    // Quiz code
+    var myQuestions = [
+      {
+        question: "What year was The Eiffel Tower built?",
+        answers: {
+          a: '1907',
+          b: '1897',
+          c: '1887'
+        },
+        correctAnswer: 'c'
+      },
+      {
+        question: "When is Bastille Day?",
+        answers: {
+          a: '12th June',
+          b: '14th July',
+          c: '29th August'
+        },
+        correctAnswer: 'b'
+      }
+    ];
+    
+    var quizContainer = document.getElementById('quiz');
+    var resultsContainer = document.getElementById('results');
+    var submitButton = document.getElementById('submit');
+    
+    generateQuiz(myQuestions, quizContainer, resultsContainer, submitButton);
+    
+    function generateQuiz(questions, quizContainer, resultsContainer, submitButton){
+    
+      function showQuestions(questions, quizContainer){
+        // we'll need a place to store the output and the answer choices
+        var output = [];
+        var answers;
+    
+        // for each question...
+        for(var i=0; i<questions.length; i++){
+          
+          // first reset the list of answers
+          answers = [];
+    
+          // for each available answer...
+          for(letter in questions[i].answers){
+    
+            // ...add an html radio button
+            answers.push(
+              '<label>'
+                + '<input type="radio" name="question'+i+'" value="'+letter+'">'
+                + letter + ': '
+                + questions[i].answers[letter]
+              + '</label>'
+            );
+          }
+    
+          // add this question and its answers to the output
+          output.push(
+            '<div class="question">' + questions[i].question + '</div>'
+            + '<div class="answers">' + answers.join('') + '</div>'
+          );
+        }
+    
+        // finally combine our output list into one string of html and put it on the page
+        quizContainer.innerHTML = output.join('');
+      }
+    
+    
+      function showResults(questions, quizContainer, resultsContainer){
+        
+        // gather answer containers from our quiz
+        var answerContainers = quizContainer.querySelectorAll('.answers');
+        
+        // keep track of user's answers
+        var userAnswer = '';
+        var numCorrect = 0;
+        
+        // for each question...
+        for(var i=0; i<questions.length; i++){
+    
+          // find selected answer
+          userAnswer = (answerContainers[i].querySelector('input[name=question'+i+']:checked')||{}).value;
+          
+          // if answer is correct
+          if(userAnswer===questions[i].correctAnswer){
+            // add to the number of correct answers
+            numCorrect++;
+            
+            // color the answers green
+            answerContainers[i].style.color = 'lightgreen';
+          }
+          // if answer is wrong or blank
+          else{
+            // color the answers red
+            answerContainers[i].style.color = 'red';
+          }
+        }
+    
+        // show number of correct answers out of total
+        resultsContainer.innerHTML = numCorrect + ' out of ' + questions.length;
+      }
+    
+      // show questions right away
+      showQuestions(questions, quizContainer);
+      
+      // on submit, show results
+      submitButton.onclick = function(){
+        showResults(questions, quizContainer, resultsContainer);
+      }
+    
+    }
