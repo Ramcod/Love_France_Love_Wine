b600c71c6cbb0046b098e11d003a81ecd462c9a0
Ramcod <ramcoding9@gmail.com>
2022-02-02 12:53:53 +0000

    Correct API key error and regenerate

--- assets/js/main.js
+++ assets/js/main.js
@@ -89,7 +89,7 @@ const popup = document.querySelector(".popup-background");
 const close = document.querySelector(".popup-exit");
 
 button.addEventListener("click", () => {
-  popup.style.display = "block";
+	popup.style.display = "block";
 });
 
 close.addEventListener("click", () => {
